5ce7e4ebcccd18798fffe35ef6f4d9a34d11d410
Kambale Muhindo <kambale.muhindo@scania.com>
2023-11-27 12:36:57 +0100

    added function for fetching all data from backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: []
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch('/api/getTasks');
+      const body = await response.json();
+      this.setState({ tasks: body }); // Fix: Use 'tasks' instead of 'clients'
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Tasks</h2>
+              {tasks.map(task => (
+                  <div key={task.id}>
+                    {task.name} {task.description} ({task.created})
+                  </div>
+              ))}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/taskList.js
@@ -0,0 +1,79 @@
+
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+import { Link } from 'react-router-dom';
+
+class taskList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {tasks: []};
+        this.remove = this.remove.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('api/getTasks')
+            .then(response => response.json())
+            .then(data => this.setState({tasks: data}));
+    }
+
+    async remove(id) {
+        await fetch(`/deleteTask/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedTasks = [...this.state.tasks].filter(i => i.id !== id);
+            this.setState({tasks: updatedTasks});
+        });
+    }
+
+    render() {
+        const {tasks, isLoading} = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        const taskList = tasks.map(task => {
+            return <tr key={task.id}>
+                <td style={{whiteSpace: 'nowrap'}}>{task.name}</td>
+                <td>{task.email}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/clients/" + task.id}>Edit</Button>
+                        <Button size="sm" color="danger" onClick={() => this.remove(task.id)}>Delete</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
+
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" tag={Link} to="/clients/new">Add Task</Button>
+                    </div>
+                    <h3>Tasks</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="30%">Task</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {taskList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+
+    }
+
+}
+export default taskList;
